4d503b1f0b759313d3c1b052be425a7fab47f409
Pradeep Kumar <vpradeepkumar94@gmail.com>
2017-07-22 14:40:39 +0530

    intitial check in

---
+++ Selenium Learnings/src/com/constantss/java/GlobalProperties.java
@@ -0,0 +1,8 @@
+package com.constantss.java;
+
+public class GlobalProperties {
+	
+	final public static String Application_URL= "http://www.seleniumeasy.com/test/basic-first-form-demo.html";
+	
+
+}
