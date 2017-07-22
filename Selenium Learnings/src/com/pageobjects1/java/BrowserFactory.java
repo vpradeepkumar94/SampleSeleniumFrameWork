33c007632a5419510e938ac7f96fddbe83a2cc0d
Pradeep Kumar <vpradeepkumar94@gmail.com>
2017-07-22 14:47:41 +0530

    refactored

--- Selenium Learnings/src/com/pageobjects1/java/BrowserFactory.java
+++ Selenium Learnings/src/com/pageobjects1/java/BrowserFactory.java
@@ -9,68 +9,68 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class BrowserFactory {
-	  public static  HashMap<String,WebDriver> Drivers = new HashMap<String,WebDriver>();
-      
-      public static  WebDriver Driver;
-	  
-      public static WebDriver getDriver() throws Exception {
-    	  if(Driver == null) {
-              throw new Exception("The WebDriver browser instance was not initialized. You should first call the method InitBrowser.");
-    	  }
-    	  else {
-    		 return Driver;
-    	  }
-	  }
+	public static HashMap<String, WebDriver> Drivers = new HashMap<String, WebDriver>();
 
-      public static void initBrowser(String browserName)
-      {          
-    	  switch (browserName.toLowerCase())
-          {
-              case "firefox":
-                  if (Driver == null)
-                  { 
-                // System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "//Drivers//geckodriverold.exe");
-            		Driver = new FirefoxDriver();
-                	Drivers.put("firefox",Driver);
-                  }
-                  break;
+	public static WebDriver Driver;
 
-              case "IE":
-                  if (Driver == null)
-                  {
-                      Driver = new InternetExplorerDriver();
-                      Drivers.put("IE",Driver);
-                  }
-                  break;
+	public static WebDriver getDriver() throws Exception {
+		if (Driver == null) {
+			throw new Exception(
+					"The WebDriver browser instance was not initialized. You should first call the method InitBrowser.");
+		} else {
+			return Driver;
+		}
+	}
 
-              case "chrome":
-                  if (Driver == null)
-                  {
-                	  try  {
-						 	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//Drivers//chromedriver2.30.exe");
-						 	 Driver = new ChromeDriver();
-						 	Drivers.put("Chrome", Driver);
-					 }catch (Exception e) {
-							System.out.println("Unable to create Driver Instance for Browser "+ browserName + e);
-					 }
-                  }
-                  break;
-          default:
-					System.out.println("Unable to create Driver Instance for Browser "+ browserName );
-			 }            
-      }
+	public static void initBrowser(String browserName) {
+		switch (browserName.toLowerCase()) {
+		case "firefox":
+			if (Driver == null) {
+				System.setProperty("webdriver.gecko.driver",
+						System.getProperty("user.dir")
+								+ "//Drivers//geckodriverold.exe");
+				Driver = new FirefoxDriver();
+				Drivers.put("firefox", Driver);
+			}
+			break;
 
-      public static void LoadApplication(String url)
-      {
-          // Driver.Url = url;
-      }
+		case "IE":
+			if (Driver == null) {
+				Driver = new InternetExplorerDriver();
+				Drivers.put("IE", Driver);
+			}
+			break;
 
-      public static void CloseAllDrivers()
-      {
-          for(Map.Entry<String,WebDriver> entry: Drivers.entrySet() ) {
-        	 Drivers.get(entry.getKey()).close();
-        	 Drivers.get(entry.getKey()).quit();
-          }
-   }
+		case "chrome":
+			if (Driver == null) {
+				try {
+					System.setProperty("webdriver.chrome.driver",
+							System.getProperty("user.dir")
+									+ "//Drivers//chromedriver2.30.exe");
+					Driver = new ChromeDriver();
+					Drivers.put("Chrome", Driver);
+				} catch (Exception e) {
+					System.out
+							.println("Unable to create Driver Instance for Browser "
+									+ browserName + e);
+				}
+			}
+			break;
+		default:
+			System.out.println("Unable to create Driver Instance for Browser "
+					+ browserName);
+		}
+	}
+
+	public static void LoadApplication(String url) {
+		// Driver.Url = url;
+	}
+
+	public static void CloseAllDrivers() {
+		for (Map.Entry<String, WebDriver> entry : Drivers.entrySet()) {
+			Drivers.get(entry.getKey()).close();
+			Drivers.get(entry.getKey()).quit();
+		}
+	}
 
 }
