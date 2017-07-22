db786ac698ff9674cf7ecafc837b64e43c868a43
Pradeep Kumar <vpradeepkumar94@gmail.com>
2017-07-22 17:38:35 +0530

    Renamed consants file name

--- Selenium Learnings/src/com/constantss/java/Constants.java
+++ Selenium Learnings/src/com/constantss/java/Constants.java
@@ -1,8 +1,10 @@
 package com.constantss.java;
 
-public class GlobalProperties {
+public class Constants {
 	
 	final public static String Application_URL= "http://www.seleniumeasy.com/test/basic-first-form-demo.html";
+	final public static  String PAGE_TITLE = "Selenium demo website";
+	final public static String message = "Welcome";
 	
 
 }
